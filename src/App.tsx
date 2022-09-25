ec74a3a5bb7d986f004b1275fea7732c0a8f4f38
YoommY <seeun0792@gmail.com>
2022-09-26 01:01:14 +0900

    Feat : useInput hook 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
+import { useDispatch } from 'react-redux';
+import useInput from './hooks/useInput';
+import { dispatchAddToDo } from './redux/store';
 
 function App() {
-  return <div className="App">hello</div>;
+  const { text, onChange, setText } = useInput();
+  const dispatch = useDispatch();
+
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setText('');
+
+    if (text.trim()) {
+      dispatch(dispatchAddToDo(text));
+    }
+  }
+  return (
+    <>
+      <h1>TODO</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          placeholder="할 일을 입력하세요"
+          value={text}
+          onChange={onChange}
+        />
+        <button>+</button>
+      </form>
+    </>
+  );
 }
 
 export default App;
